f4a263a183091dc723509fef0391ee6fceeb0e0f
Jason Silberman <j@j99.co>
2015-06-30 13:04:38 -0700

    opening of the bank

---
+++ modules/bank/bank.php
@@ -0,0 +1,21 @@
+<? namespace Titanium\Modules; defined('PATH') or die;
+
+class Bank {
+  protected static $information = [];
+
+  public static function set($key, $value) {
+    static::$information[$key] = $value;
+  }
+
+  public static function get($key, $fallback = false) {
+    if (isset(static::$information[$key])) {
+      return static::$information[$key];
+    } else {
+      return $fallback;
+    }
+  }
+
+  public static function unset($key) {
+    unset(static::$information[$key]);
+  }
+}
