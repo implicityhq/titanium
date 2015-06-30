14b75e624a7a3f44a8dcba05a2426b4405a1815a
Jason Silberman <j@j99.co>
2015-06-30 15:24:49 -0700

    selectAllWhere using email

--- app/controllers/main.php
+++ app/controllers/main.php
@@ -8,7 +8,7 @@ class MainController {
     $chug_test = new Pipe(Bank::get('db'));
     $chug_test->table = 'chug_test';
 
-    $results = $chug_test->selectAll();
+    $results = $chug_test->selectAllWhere([['email', '~', '%oo%']]);
     $rs->render(json_encode($results), ['Content-Type' => 'application/json']);
   }
 
